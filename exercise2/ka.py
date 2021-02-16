044bcda3df78a7c6dba97b812776d59239b2a25c
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2021-02-16 22:54:30 +0100

    Adds storage of solutions at each time step

--- exercise2/ka.py
+++ exercise2/ka.py
@@ -55,18 +55,23 @@ def forward_euler(bc1, bc2, M, N, t_end):
     Returns:
         x : spatial grid
         U : solution of the heat equation at time t_end
+        solution_matrix : NxM matrix, row i is U at time ti 0 < ti < t_end
     """
 
     x, h = np.linspace(0, 1, M, retstep=True)
     t, k = np.linspace(0, t_end, N, retstep=True)
     r = k/(h**2)
     U = u0(x) # initial, t = 0
+
+    solution_matrix = np.zeros((N, M))
+    solution_matrix[0] = U
     
-    for ti in t[1:]:
+    for (i, ti) in enumerate(t[1:]):
         U[0] = g(bc1, U[0], ti)
         U[-1] = g(bc2, U[-1], ti)
         U[1:-1] = U[1:-1] + r * (U[:-2] -2*U[1:-1] +U[2:])
-    return x, U
+        solution_matrix[i] = U
+    return x, U, solution_matrix
 
 
 def backward_euler(bc1, bc2, M, N, t_end):
@@ -82,6 +87,7 @@ def backward_euler(bc1, bc2, M, N, t_end):
     Returns:
         x : spatial grid
         U : solution of the heat equation at time t_end
+        solution_matrix : NxM matrix, row i is U at time ti 0 < ti < t_end
     """
 
     x, h = np.linspace(0, 1, M, retstep=True)
@@ -89,16 +95,20 @@ def backward_euler(bc1, bc2, M, N, t_end):
     r = k/(h**2)
     U = u0(x) # initial, t = 0
 
+    solution_matrix = np.zeros((N, M))
+    solution_matrix[0] = U
+
     m = M-2
     diag = np.repeat(1+2*r, m)
     offdiag = np.repeat(-r, m-1)
     A = diags([diag, offdiag, offdiag], [0,1,-1])
-    for ti in t[1:]:
+    for (i, ti) in enumerate(t[1:]):
         b = U[1:-1]
         b[0] += r*g(bc1, U[0], ti)
         b[-1] += r*g(bc2, U[-1], ti)
         U[1:-1] = np.linalg.solve(A.toarray(), b) # There is probably a solver made for sparse arrays which is better
-    return x, U
+        solution_matrix[i] = U
+    return x, U, solution_matrix
 
 
 def crank_nicolson(bc1, bc2, M, N, t_end):
@@ -114,6 +124,7 @@ def crank_nicolson(bc1, bc2, M, N, t_end):
     Returns:
         x : spatial grid
         U : solution of the heat equation at time t_end
+        solution_matrix : NxM matrix, row i is U at time ti 0 < ti < t_end
     """
 
     x, h = np.linspace(0, 1, M, retstep=True)
@@ -121,19 +132,23 @@ def crank_nicolson(bc1, bc2, M, N, t_end):
     r = k/(h**2)
     U = u0(x) # initial, t = 0
 
+    solution_matrix = np.zeros((N, M))
+    solution_matrix[0] = U
+
     m = M-2
     diag = np.repeat(1+r, m)
     offdiag = np.repeat(-r/2, m-1)
     A = diags([diag, offdiag, offdiag], [0,1,-1])
-    for ti in t[1:]:
+    for (i, ti) in enumerate(t[1:]):
         b = (r/2)*U[:-2] + (1-r)*U[1:-1] + (r/2)*U[2:]
         b[0] += (r/2)*g(bc1, U[0], ti)
         b[-1] += (r/2)*g(bc2, U[-1], ti)
         U[1:-1] = np.linalg.solve(A.toarray(), b) # There is probably a solver made for sparse arrays which is better
-    return x, U
+        solution_matrix[i] = U
+    return x, U, solution_matrix
 
 
-def test_method(method, M, N):
+def test_method(method, M, N, t_end):
     """
     Do a testrun and plot results for a numerical solver of the heat equation
 
@@ -141,31 +156,27 @@ def test_method(method, M, N):
         method : the function name of the method to be tested e.g. forward_euler
         M : number of spacial grid points
         N : number of time grid points
+        t_end : end time
     """
+
     bc1 = BoundaryCondition(BoundaryCondition.NEUMANN, 0)
     bc2 = BoundaryCondition(BoundaryCondition.NEUMANN, 0)
-    x, U_0 = method(bc1, bc2, M, N, 0.0)
-    x, U_1 = method(bc1, bc2, M, N, 0.01)
-    x, U_2 = method(bc1, bc2, M, N, 0.02)
-    x, U_3 = method(bc1, bc2, M, N, 0.03)
-    x, U_4 = method(bc1, bc2, M, N, 0.1)
-    # plot
-    plt.title(method.__name__)
-    plt.plot(x, u0(x))
-    plt.plot(x, U_0, ".")
-    plt.plot(x, U_1, ".")
-    plt.plot(x, U_2, ".")
-    plt.plot(x, U_3, ".")
-    plt.plot(x, U_4, ".")
-    plt.show()
+    x, U_final, solutions = method(bc1, bc2, M, N, t_end)
     
+    sample_times = np.linspace(0, t_end, 5)
+    for (i, t) in enumerate(sample_times):
+        plt.plot(x, solutions[i], ".",label=f"t={t}")
+    plt.title(method.__name__)
+    plt.legend()
+    plt.show()
+
 
 if __name__ == "__main__":
     ## Test forward Euler ##
-    test_method(forward_euler, 100, 10000)
+    test_method(forward_euler, 100, 100, 1)
 
     ## Test backward Euler
-    test_method(backward_euler, 100, 100)
+    test_method(backward_euler, 100, 100, 1)
 
     ## Test Crank-Nicolson
-    test_method(crank_nicolson, 100, 100)
+    test_method(crank_nicolson, 100, 100, 1)
