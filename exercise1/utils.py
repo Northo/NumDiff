028d841b877d284036af5f244450fc717f03c394
Thorvald Ballestad <thorvalb@stud.ntnu.no>
2021-02-19 15:43:35 +0100

    Implement variable step length

--- exercise1/utils.py
+++ exercise1/utils.py
@@ -1,17 +1,17 @@
+import warnings
+from dataclasses import dataclass
 from enum import Enum
 from functools import partial
-from dataclasses import dataclass
 
 import numpy as np
 from scipy.integrate import quad
 
 
-import warnings
-
 class BCType(Enum):
     DIRICHLET = 1
     NEUMANN = 2
     VALUE = 1  # Alias for Dirichlet.
+
 
 @dataclass
 class BC:
@@ -23,6 +23,7 @@ class BC:
 
     def is_dirichlet(self):
         return self.type == BCType.DIRICHLET
+
 
 DEFAULT_BCs = [BC(BCType.VALUE, 0), (BCType.NEUMANN, 1)]
 
@@ -83,10 +84,13 @@ def interpolation_continuation(V, x_min=0, x_max=1):
 
     return V_continous
 
+
 error_functions = {
     "L2 discrete": lambda U, u, x: L2_discrete_error(U, u(x)),
     "L2 continous step": lambda U, u, x: L2_continous_error(step_continuation(U), u),
-    "L2 continous interpolation": lambda U, u, x: L2_continous_error(interpolation_continuation(U), u)
+    "L2 continous interpolation": lambda U, u, x: L2_continous_error(
+        interpolation_continuation(U), u
+    ),
 }
 
 
@@ -97,32 +101,32 @@ def find_errors(M_list, f, BCs, error_functions=error_functions):
     of errors for that norm over M_list."""
     # Error functions to measure.
     # Each function must have the call signature f(U:ndarray, u:function, x:ndarray).
-    errors = {error_name:[] for error_name, error_function in error_functions.items()}
+    errors = {error_name: [] for error_name, error_function in error_functions.items()}
     for M in M_list:
         A, F, x = generate_problem(f, M, BCs)
         U = solve_handle(A, F)
         analytical = u(BCs)
         for error_name, error_function in error_functions:
-            errors[error_name].append(
-                error_function(U, analytical, x)
-            )
+            errors[error_name].append(error_function(U, analytical, x))
     return errors
+
 
 def find_errors_np(M_list, f, BCs, error_functions=error_functions):
     """Find errors for given values of M for different norms"""
     # Error functions to measure.
     # Each function must have the call signature f(U:ndarray, u:function, x:ndarray).
 
-    errors = np.empty((len(error_functions)+1, len(M_list)))
+    errors = np.empty((len(error_functions) + 1, len(M_list)))
     errors[0, :] = M_list
     for j, M in enumerate(M_list):
         A, F, x = generate_problem(f, M, BCs)
         U = solve_handle(A, F)
         analytical = u(BCs)
         for i, (error_name, error_function) in enumerate(error_functions.items()):
-            errors[i+1, j] = error_function(U, analytical, x)
+            errors[i + 1, j] = error_function(U, analytical, x)
 
     return errors
+
 
 def write_errors_file(filename, Ms, errors):
     """Write errors defined in the format returned by find_erros to a file"""
@@ -135,6 +139,7 @@ def write_errors_file(filename, Ms, errors):
             file.write(str(Ms[i]))
             for error in errors:
                 file.write("\t" + f"{errors[error][i]:8.3f}")
+
 
 def existence_neumann_neumann(F, h, sigma0, sigma1):
     """Condition of existence for a neumann-neumann problem, discrete formulation."""
@@ -168,16 +173,15 @@ def generate_problem(f, M, BCs=DEFAULT_BCs):
     # For later convenience, we will define inner, which is simply the range of x-values we actually use.
     inner = range(
         int(bc_left.is_dirichlet()),
-        M + 1  # Add one because range is excluding endpoint.
-        + int(
-            bc_right.is_neumann()
-        ),
+        M
+        + 1  # Add one because range is excluding endpoint.
+        + int(bc_right.is_neumann()),
     )
 
     # Apply values common for all BCs.
-    diagonal = np.full(M + nn, -2/h**2)
-    upper_diagonal = np.ones(M + nn - 1)/h**2
-    lower_diagonal = np.ones(M + nn - 1)/h**2
+    diagonal = np.full(M + nn, -2 / h ** 2)
+    upper_diagonal = np.ones(M + nn - 1) / h ** 2
+    lower_diagonal = np.ones(M + nn - 1) / h ** 2
     A = np.diag(diagonal) + np.diag(upper_diagonal, k=1) + np.diag(lower_diagonal, k=-1)
     F = f(x[inner])
 
@@ -201,13 +205,71 @@ def generate_problem(f, M, BCs=DEFAULT_BCs):
             # Solution only determined up to a constant.
             # We set u(0) = 0 as extra condition.
             A[:, 0] = 0
-            warnings.warn("Two Neumann conditions renders the IVP ill-posed. u(0) = 0 was imposed.")
+            warnings.warn(
+                "Two Neumann conditions renders the IVP ill-posed. u(0) = 0 was imposed."
+            )
     elif bc_left.is_dirichlet():
         F[0] -= bc_left.value / h ** 2
     else:
         raise Exception("Unknown boundary condition type.")
 
     return A, F, x[inner]
+
+
+def generate_problem_variable_step(f, x, BCs=[BC(value=1), BC(value=1)]):
+    """Generate probelm for variable step size x.
+    Currently only implmented for Dirichlet BCs"""
+    left_bc, right_bc = BCs
+    if not left_bc.is_dirichlet() or not right_bc.is_dirichlet():
+        raise NotImplementedError("Currently only support for Dirichlet-Dirichlet.")
+
+    ### This is how the fuck variable step length works ###
+    # There are M internal points, U_0 and U_(M+1) are on the boundary.
+    # Let's say we have the x-points as below, where the number indicate
+    # the index, and h the distance between.
+    #
+    # 0 --h0-- 1 --h1-- 2 --h2-- ... --h(M-2)-- M-1 --h(M-1)-- M --hM-- M+1
+    # As written in Owren p. 69 (nice...), we have, for a point P
+    # where we denote q = p+1, r = p-1:
+    # r --hr-- p --hp-- q
+    # U''_p = 2/(hr + hp) * [ (U_q - U_p)/hp - (U_p - U_r)/hr ]
+    #       = 2/(hr + hp) * [  U_r/hr - U_p (1/hp + 1/hr) + U_q/hp ].
+    ### End of how the fuck variable step length works ###
+
+    # We will now build the "inner" part of our matrix A, the part
+    # corresponding with U1 .... UM. The parts that deal with
+    # BCs will be dealt with later.
+    h = x[1:] - x[:-1]
+    # For cleaner code, define h_back and h_front
+    # At some index p, h_front gives h to next point, h_back to previous point.
+    h_back, h_front = h[:-1], h[1:]
+    # Note that (1/a + 1/b) / (a + b) = 1/(ab).
+    diagonal = -2 / (h_back * h_front)
+    upper_diagonal = 2 / (h_back + h_front) / h_front
+    lower_diagonal = 2 / (h_back + h_front) / h_back
+    # upper_ and lower_diagonal must be shorter than diagonal.
+    # Stare hard at the slicing, it makes sense.
+    A = (
+        np.diag(diagonal)
+        + np.diag(upper_diagonal[:-1], k=1)
+        + np.diag(lower_diagonal[1:], k=-1)
+    )
+    F = f(x[1:-1])
+
+    # Time to deal with BCs.
+    # For Dirichelt-BCs, we do not have to change A, we simply modify F.
+    # For Neumann-BCs, we extend the size of our system.
+    # For two Neumann-BCs, we must be really careful, as the problem is ill-posed.
+
+    # Right BC:
+    # We move the coefficient corresponding to U_(M+1)
+    # in the expression above to the other side (F).
+    F[-1] -= right_bc.value * 2 / (h_front[-1] + h_back[-1]) / h_front[-1]
+
+    # Left BC:
+    F[0] -= left_bc.value * 2 / (h_front[0] + h_back[0]) / h_back[0]
+
+    return A, F, x[1:-1]
 
 
 def f(x):
@@ -242,7 +304,7 @@ def u(BCs=DEFAULT_BCs):
 
     ## Left bounary ##
     if bc_left.is_dirichlet():
-        c1 = 1/(4 * np.pi**2) + bc_left.value
+        c1 = 1 / (4 * np.pi ** 2) + bc_left.value
     elif bc_left.is_neumann():
         c2 = bc_left.value
     else:
@@ -251,20 +313,27 @@ def u(BCs=DEFAULT_BCs):
     ## Right boundary ##
     if bc_right.is_dirichlet():
         if bc_left.is_dirichlet():
-            c2 = -c1 - 1/12*(2*np.pi**2-3)/np.pi**2 + bc_right.value
+            c2 = -c1 - 1 / 12 * (2 * np.pi ** 2 - 3) / np.pi ** 2 + bc_right.value
         else:
-            c1 = -c2 - 1/12*(2*np.pi**2-3)/np.pi**2 + bc_right.value
+            c1 = -c2 - 1 / 12 * (2 * np.pi ** 2 - 3) / np.pi ** 2 + bc_right.value
     elif bc_right.is_neumann():
         if bc_left.is_neumann():
             warnings.warn("Ill-posed problem! Imposing u(0)=0.")
-            c1 = 1 / (4 * np.pi**2)
-        c2 = -1/2 + bc_right.value
+            c1 = 1 / (4 * np.pi ** 2)
+        c2 = -1 / 2 + bc_right.value
     else:
         raise Exception("Unknown boundary condition type.")
 
-
     def _u(x):
-        return c2*x + c1 + 1/12*(2*np.pi**2*x**3 - 3*np.cos(2*np.pi*x))/np.pi**2
+        return (
+            c2 * x
+            + c1
+            + 1
+            / 12
+            * (2 * np.pi ** 2 * x ** 3 - 3 * np.cos(2 * np.pi * x))
+            / np.pi ** 2
+        )
+
     return _u
 
 
