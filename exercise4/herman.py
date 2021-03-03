82d7006f7e7791a8b4e139b67a369a82576f1533
Herman Sletmoen <hermansletmoen@gmail.com>
2021-03-03 19:27:59 +0100

    Added analytical solution to exercise 4

--- exercise4/herman.py
+++ exercise4/herman.py
@@ -26,27 +26,37 @@ def backward_euler(t, A, U0):
 def crank_nicholson(t, A, U0):
     return theta_method(t, A, U0, theta=1/2)
 
-def animate_wave(x, U, t):
-    def animate(i):
-        ax.clear()
-        ax.plot(x, U[i,:])
-
+def animate_solution(x, t, U, u):
     fig, ax = plt.subplots()
-    ani = matplotlib.animation.FuncAnimation(fig, animate, interval=20)
+
+    ymin = np.min((np.min(U), np.min(u)))
+    ymax = np.max((np.max(U), np.max(u)))
+    ax.set_ylim(ymin, ymax)
+    ax.set_xlim(x[0], x[-1])
+
+    graph1, = ax.plot([], [], linewidth=1, color="red")
+    graph2, = ax.plot([], [], linewidth=5, color="black")
+
+    def animate(i):
+        graph1.set_data(x, U[i,:])
+        graph2.set_data(x, u[i,:])
+
+    ani = matplotlib.animation.FuncAnimation(fig, animate, interval=0)
     plt.show()
 
 def main():
-    N = 50
+    N = 400
     x, h = np.linspace(-1, +1, N, retstep=True)
+    t, k = np.linspace(0, 1, 500, retstep=True)
     N = np.shape(x)[0]
+
+    print(f"k/h^2 = {k/h**2}")
 
     A = np.zeros((N, N))
     stencil1 = np.array([-1, 0, +1]) / (2*h)
     stencil3 = np.array([-1/8, 0, +3/8, 0, -3/8, 0, +1/8]) / h**3
     relinds1 = [i - (len(stencil1)-1)//2 for i in range(0, len(stencil1))]
     relinds3 = [i - (len(stencil3)-1)//2 for i in range(0, len(stencil3))]
-    print(relinds1)
-    print(relinds3)
     for i in range(0, N):
         inds1 = [(i + relind1) % N for relind1 in relinds1]
         inds3 = [(i + relind3) % N for relind3 in relinds3]
@@ -54,10 +64,9 @@ def main():
         A[i,inds3] -= stencil3
 
     U0 = np.sin(np.pi*x)
-
-    t = np.linspace(0, 2, 200)
     U = crank_nicholson(t, A, U0)
+    u = np.sin(np.pi*(x-t.reshape(-1,1)))
 
-    animate_wave(x, U, t)
+    animate_solution(x, t, U, u)
 
 main()
