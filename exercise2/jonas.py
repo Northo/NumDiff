f2671591e312024dba498700281074ff49b260ef
Jonas Bueie <jonasbue@stud.ntnu.no>
2021-03-09 18:18:46 +0100

    Implements Backward Euler and Crank-Nicolson solvers with Neumann-Neumann boundary conditions.

--- exercise2/jonas.py
+++ exercise2/jonas.py
@@ -29,7 +29,7 @@ def backward_euler(f, M, N, t_end, g_0, g_1, bc="d"):
     r = k/h**2
     
     if bc == "d":
-        x = np.linspace(h, 1-h, M)    # Not including x = 0 if there are
+        x = np.linspace(h, 1-h, M)  # Not including x = 0 if there are
                                     # Dirichlet boundary condtitions
     elif bc == "n":                 # For Neumann boundary conditions,
         x = np.linspace(0, 1, M+2)  # x = 0 must be included.
@@ -39,26 +39,30 @@ def backward_euler(f, M, N, t_end, g_0, g_1, bc="d"):
     sol[0] = f(x.copy())
 
     main_diag = np.full(len(x), 1+2*r)
-    off_diag = np.full(len(x)-1, -r)
-
-    A = np.diag(off_diag, -1) + np.diag(off_diag, 1) + np.diag(main_diag, 0)
+    off_diag_upper = np.full(len(x)-1, -r)
+    off_diag_lower = np.full(len(x)-1, -r)
 
     if bc == "d":
+        A = np.diag(off_diag_lower, -1) + np.diag(off_diag_upper, 1) + np.diag(main_diag, 0)
         for i in range(N-1):
             b = sol[i]
-            b[0] += r*g_0(t[i+1])
+            b[0] += r*g_0(f, t[i+1])
             b[-1] += r*g_1(f, t[i+1])
             sol[i+1] = linalg.solve(A, b)
         x = np.insert(x, 0, 0)
-        x = np.insert(x, -1, 1)
+        x = np.insert(x, len(sol[0]), 1)
         sol = np.insert(sol, 0, f(0), axis=1)
-        sol = np.insert(sol, -1, f(1), axis=1)
+        sol = np.insert(sol, len(sol[0]), f(1), axis=1)
     elif bc == "n":
+        main_diag[0] = 1-r
+        main_diag[-1] = 1+r
+        off_diag_upper[0] = r
+        A = np.diag(off_diag_lower, -1) + np.diag(off_diag_upper, 1) + np.diag(main_diag, 0)
         for i in range(N-1):
             b = sol[i]
-            b[0] -= 2*r*h*g_0(t[i+1])
-            b[-1] += 2*r*h*g_1(f, t[i+1])
-        sol[i+1] = linalg.solve(A, b)
+            b[0] -= 2 * r*r * h * g_0(f, t[i+1])
+            b[-1] += 2 * r*r * h * g_1(f, t[i+1])
+            sol[i+1] = linalg.solve(A, b)
     return x, sol
 
 def crank_nicolson(f, M, N, t_end, g_0, g_1, bc="d"):
@@ -89,33 +93,46 @@ def crank_nicolson(f, M, N, t_end, g_0, g_1, bc="d"):
         x = np.linspace(h, 1-h, M)    # Not including x = 0 if there are
                                     # Dirichlet boundary condtitions
     elif bc == "n":                 # For Neumann boundary conditions,
-        x = np.linspace(0, 1, M+1)  # x = 0 must be included.
+        x = np.linspace(0, 1, M+2)  # x = 0 must be included.
 
     t = np.linspace(0, t_end, N)
     sol = np.zeros((N, len(x)))
     sol[0] = f(x.copy())
 
     main_diag = np.full(len(x), 1+r)
-    off_diag = np.full(len(x)-1, -r/2)
+    off_diag_upper = np.full(len(x)-1, -r/2)
+    off_diag_lower = np.full(len(x)-1, -r/2)
 
-    A = np.diag(off_diag, -1) + np.diag(off_diag, 1) + np.diag(main_diag, 0)
     if bc == "d":
+        A = np.diag(off_diag_lower, -1) + np.diag(off_diag_upper, 1) + np.diag(main_diag, 0)
         for i in range(N-1):
-            b = np.zeros(len(sol[i]))
+            # b is the right hand side of the equation Au=b.
+            b = np.zeros(len(x))
             b[1:-1] = 0.5*r*sol[i,:-2] + (1-r)*sol[i,1:-1] + 0.5*r*sol[i,2:]
-            b[0] += 0.5*r*g_0(t[i+1])
+            b[0] += 0.5*r*g_0(f, t[i+1])
             b[-1] += 0.5*r*g_1(f, t[i+1])
+        
+            # sol[i,:] is the unknown vector u at time step i.
             sol[i+1] = linalg.solve(A, b)
+
+        # Insert values for f(0) and f(1) in sol and x, for all times.
         x = np.insert(x, 0, 0)
         sol = np.insert(sol, 0, f(0), axis=1)
-        x = np.insert(x, -1, 1)
-        sol = np.insert(sol, -1, f(1), axis=1)
+        x = np.insert(x, len(sol[i]), 1)
+        sol = np.insert(sol, len(sol[i]), f(1), axis=1)
+        print(sol[i])
+        print(x)
     elif bc == "n":
+        main_diag[0] = 1+r/2
+        main_diag[-1] = 1+r/2
+        off_diag_upper[0] = -r/2
+        off_diag_lower[-1] = -r/2
+        A = np.diag(off_diag_lower, -1) + np.diag(off_diag_upper, 1) + np.diag(main_diag, 0)
         for i in range(N-1):
-            b = np.zeros(len(sol[i]))
-            b[1:-1] = sol[i,:-1] - 2*sol[i,1:-1] + sol[i,2:]
-            b[0] = g_0(t[i+1])
-            b[-1] += 0.5*r*g_1(f, t[i+1])
+            b = np.zeros(len(x))
+            b[1:-1] = 0.5*r*sol[i,:-2] + (1-r)*sol[i,1:-1] + 0.5*r*sol[i,2:]
+            b[0] = sol[i,0] + 0.5*r*(sol[i,1] -sol[i,0]) - 2*r*h*g_0(f, t[i+1])
+            b[-1] = sol[i,-1] + 0.5*r*(sol[i,-2] - sol[i,-1]) - 2*r*h*g_1(f, t[i+1])
             sol[i+1] = linalg.solve(A, b)
     return x, sol
 
@@ -123,7 +140,7 @@ def animate(i, x, U, curve):
     curve.set_data(x, U[i])
     return curve 
 
-def animate_time_development(x, U):
+def animate_time_development(x, U, title):
     '''
     Animates values of U developing in N time steps.
     Variables:
@@ -133,6 +150,7 @@ def animate_time_development(x, U):
     '''
     fig = plt.figure()
     ax = plt.axes(xlim=(x[0], x[-1]), ylim=(0,f(1)))
+    ax.set_title(title)
     curve, = ax.plot(x, U[0])
     anim = animation.FuncAnimation(fig, animate, interval=200, fargs=(x, U, curve))
     return anim
@@ -146,8 +164,8 @@ def g(x):
 def burger(x):
     return np.exp(-400*(x - 0.5)**2)
 
-def g_0(t):
-    return 0
+def g_0(f, t):
+    return f(0)
 
 def g_1(f, t):
     return f(1)
@@ -205,19 +223,20 @@ def solve_order_2(f, m, h, alpha, sigma):
 #plt.plot(x, U_euler[2])
 #plt.show()
 
-m = 50
+m = 100
 N = 100
 t_end = 1
 h = 1/(m+1)
 alpha = 0
 sigma = 0
 
-x_be, U_be = backward_euler(f, m, N, t_end, g_0, g_1, bc="d")
-x_cn, U_cn = crank_nicolson(f, m, N, t_end, g_0, g_1, bc="d")
+x_be, U_be = backward_euler(f, m, N, t_end, g_0, g_1, bc="n")
+x_cn, U_cn = crank_nicolson(f, m, N, t_end, g_0, g_1, bc="n")
 
-anim_be = animate_time_development(x_be, U_be)
+
+anim_be = animate_time_development(x_be, U_be, "Soltuion with backward Euler")
 plt.show()
 
-anim_cn = animate_time_development(x_cn, U_cn)
+anim_cn = animate_time_development(x_cn, U_cn, "Soltuion with Crank-Nicolson")
 plt.show()
     
