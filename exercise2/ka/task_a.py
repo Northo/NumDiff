9232ef9da2429591a4f50d40632835fc22a88515
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2021-03-15 15:15:06 +0100

    Mainly more informative data file naming

--- exercise2/ka/task_a.py
+++ exercise2/ka/task_a.py
@@ -7,28 +7,30 @@ OUT_DIR = "../../report/exercise2/data_ka/"
 def make_discrete_convergence_plots(u0, bc1, bc2):
     M_ref = 1000
     N = 100
+    t_end = 1
     ref_grid = Grid(Grid.UNIFORM, np.linspace(0, 1, M_ref))
-    outpath=f"{OUT_DIR}2a_BE_discrete_err.dat"
+    outpath=f"{OUT_DIR}2a_BE_discrete_err_N{N}_Mref{M_ref}_tend{t_end}.dat"
     discrete_convergence_plot_M_ref(
-        backward_euler, ref_grid, bc1, bc2, u0, N, 0.5, plot=True, outpath=outpath
+        backward_euler, ref_grid, bc1, bc2, u0, N, t_end, plot=True, outpath=outpath
     )
-    outpath=f"{OUT_DIR}2a_CN_discrete_err.dat"
+    outpath=f"{OUT_DIR}2a_CN_discrete_err_N{N}_Mref{M_ref}_tend{t_end}.dat"
     discrete_convergence_plot_M_ref(
-        crank_nicolson, ref_grid, bc1, bc2, u0, N, 0.5, plot=True, outpath=outpath
+        crank_nicolson, ref_grid, bc1, bc2, u0, N, t_end, plot=True, outpath=outpath
     )
 
 
 def make_continous_convergence_plots(u0, bc1, bc2):
     M_ref = 1000
     N = 100
+    t_end = 1
     ref_grid = Grid(Grid.UNIFORM, np.linspace(0, 1, M_ref))
-    outpath=f"{OUT_DIR}2a_BE_continous_err.dat"
+    outpath=f"{OUT_DIR}2a_BE_continous_err_N{N}_Mref{M_ref}_tend{t_end}.dat"
     continous_convergence_plot_M_ref(
-        backward_euler, ref_grid, bc1, bc2, u0, N, 0.5, plot=True, outpath=outpath
+        backward_euler, ref_grid, bc1, bc2, u0, N, t_end, plot=True, outpath=outpath
     )
-    outpath=f"{OUT_DIR}2a_CN_continous_err.dat"
+    outpath=f"{OUT_DIR}2a_CN_continous_err_N{N}_Mref{M_ref}_tend{t_end}.dat"
     continous_convergence_plot_M_ref(
-        crank_nicolson, ref_grid, bc1, bc2, u0, N, 0.5, plot=True, outpath=outpath
+        crank_nicolson, ref_grid, bc1, bc2, u0, N, t_end, plot=True, outpath=outpath
     )
 
 
@@ -42,11 +44,13 @@ if __name__ == "__main__":
     bc1 = BoundaryCondition(BoundaryCondition.NEUMANN, 0)
     bc2 = BoundaryCondition(BoundaryCondition.NEUMANN, 0)
 
-    make_discrete_convergence_plots(u0, bc1, bc2)
-    make_continous_convergence_plots(u0, bc1, bc2)
+    # Commented out to avoid accidental data overwrite
+    #make_discrete_convergence_plots(u0, bc1, bc2)
+    #make_continous_convergence_plots(u0, bc1, bc2)
 
     grid = Grid(Grid.UNIFORM, np.linspace(0, 1, 100))
-    t, U_final, sols = crank_nicolson(grid, bc1, bc2, u0, 100, 0.5)
+#    t, U_final, sols = crank_nicolson(grid, bc1, bc2, u0, 100, 0.5)
+    t, U_final, sols = backward_euler(grid, bc1, bc2, u0, 100, 0.5)
 
     # Animation
     animation = animate_time_development(grid.x, sols)
