5914e5cbfa07bd5b836a057c2776a3abb5c9ef07
Jonas Bueie <jonasbue@stud.ntnu.no>
2021-03-23 15:00:01 +0100

    Uses sparse solver in 3b for a massive speedup. Take that, Thorvald!

--- exercise3/jonas/main.py
+++ exercise3/jonas/main.py
@@ -1,4 +1,6 @@
-from scipy.sparse import diags
+#import scipy.sparse as sp
+from scipy.sparse import diags, csr_matrix
+from scipy.sparse.linalg import spsolve
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -22,75 +24,65 @@ import matplotlib.pyplot as plt
 # It's easier to flatten it and use one matrix 
 # of size N*N, where N = n*n.
 
-Nx = 30
+Nx = 100
 hx = 1/(1+Nx)
-Ny = 17
+Ny = 100
 hy = 1/(1+Ny)
 
 main_diag = np.full(Nx*Ny, -2/hx**2 - 2/hy**2)
 off_diag = np.full(Nx*Ny-1, 1/hy**2)
 super_off_diag = np.full(Ny*(Nx-1), 1/hx**2)
 
-# JONAS used SPARSE!
-A = diags([
+A = csr_matrix(diags([
     main_diag, 
     off_diag,
     off_diag,
     super_off_diag,
     super_off_diag],
     (0,1,-1,Nx,-Nx)
-)
-#print("A: \n", A.toarray())
-# THORVALD FLINCHED!
+))
 
 # xy contains all coordinates
 x = np.linspace(0, 1, Nx)
 y = np.linspace(0, 1, Ny)
-xy = np.array(np.meshgrid(x, y)) #, sparse=True)
+xy = np.meshgrid(x, y, sparse=True)
 
-# Initial conditions:
-# Define the vector U which will contain the solution.
-# Note that we use a flattened matrix (vector),
-# so the index U[i,j] is accessed with index U[i*n+j].
-B = np.zeros((Ny, Nx))
-print(B)
-
-# The value of U_ij can be calculated from (the stencil above):
-# 4*U[i,j] = U[i-1¸j] + U[i+1,j] + U[i,j-1] + U[i,j+1],
-# and every index [i,j] is accessed with U[i*n+j]
-
-# Initialize U with the boundary conditions:
-
+# Define boundary functions:
 def g_0(x):
     return 0
 
 def g_1(x):
     return np.sin(2*np.pi*x)
 
-## All values on the edges are initiated with g_0 or g_1.
-# Using a 2d matrix here, to keep the code intuitive.
-# TODO: Use sparse matrix instead.
-# y = 0:
-B[0,:Nx] = g_0(xy[0])
+# Initialize system with boundary conditions:
+# Define a vector b which holds the right 
+# hand side of the equation A*u = b.
+# Initializing b with a 2d matrix B, to keep the code intuitive.
+# After flatteni1g B, we get a matrix where the
+# element B[i,j] is accessed with index B[i*n+j].
+b = np.zeros(Ny*Nx)
 
-# x = 0 and x = 1:
-B[:,0] = g_0(xy[:,0])
-B[:,-1] = g_0(B[:,-1])
-
+# All values on the edges are initiated with g_0 or g_1.
 # y = 1:
-B[-1,:] = g_1(xy[0,-1,:])/hx**2
-#print("B: \n", np.round(B, 2))
+b[0:Nx] = g_1(x)/hy**2
+# x = 0 and x = 1:
+b[0::Nx] = g_0(y)
+b[Nx-1::Nx] = g_0(y)
+# y = 0:
+b[Nx*(Ny-1):] = g_0(x)
+#b = B.flatten()
+print(b)
 
-b = B.flatten()
+# The value of U_ij ise calculated from (the stencil above):
+# 4*U[i,j] = U[i-1¸j] + U[i+1,j] + U[i,j-1] + U[i,j+1],
+# where every index [i,j] is accessed with U[i*n+j]
+# This means that we will solve the equation 
+# Au = b.
+U = spsolve(A, b)
+U = U.reshape((Ny,Nx))
 
-#print(B)
-#print(b)
-
-U = np.linalg.solve(A.toarray(), b[::-1]).reshape((Ny,Nx))
-
-print(np.round(U,2))
-
-plt.subplot(121)
+plt.figure()
 plt.title("Numerical solution")
-plt.imshow(U)
+# Flip U because in our original array, x = 0 lies at the top.
+plt.imshow(np.flip(U, axis=1), extent=[0,1,0,1])
 plt.show()
