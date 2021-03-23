337963119317fd004ff0e700090d0ee057fd92db
Jonas Bueie <jonasbue@stud.ntnu.no>
2021-03-23 13:38:42 +0100

    Generalizes 3b to recatngular grid.

--- exercise3/jonas/main.py
+++ exercise3/jonas/main.py
@@ -22,12 +22,14 @@ import matplotlib.pyplot as plt
 # It's easier to flatten it and use one matrix 
 # of size N*N, where N = n*n.
 
-n = 16
-N = n**2
+Nx = 30
+hx = 1/(1+Nx)
+Ny = 17
+hy = 1/(1+Ny)
 
-main_diag = np.full(N, -4)
-off_diag = np.full(N-1, 1)
-super_off_diag = np.full(N-n, 1)
+main_diag = np.full(Nx*Ny, -2/hx**2 - 2/hy**2)
+off_diag = np.full(Nx*Ny-1, 1/hy**2)
+super_off_diag = np.full(Ny*(Nx-1), 1/hx**2)
 
 # JONAS used SPARSE!
 A = diags([
@@ -36,21 +38,22 @@ A = diags([
     off_diag,
     super_off_diag,
     super_off_diag],
-    (0,1,-1,n,-n)
+    (0,1,-1,Nx,-Nx)
 )
 #print("A: \n", A.toarray())
 # THORVALD FLINCHED!
 
 # xy contains all coordinates
-x = np.linspace(0, 1, n)
-y = np.linspace(0, 1, n)
+x = np.linspace(0, 1, Nx)
+y = np.linspace(0, 1, Ny)
 xy = np.array(np.meshgrid(x, y)) #, sparse=True)
 
 # Initial conditions:
 # Define the vector U which will contain the solution.
 # Note that we use a flattened matrix (vector),
 # so the index U[i,j] is accessed with index U[i*n+j].
-B = np.zeros((n,n))
+B = np.zeros((Ny, Nx))
+print(B)
 
 # The value of U_ij can be calculated from (the stencil above):
 # 4*U[i,j] = U[i-1¸j] + U[i+1,j] + U[i,j-1] + U[i,j+1],
@@ -65,16 +68,17 @@ def g_1(x):
     return np.sin(2*np.pi*x)
 
 ## All values on the edges are initiated with g_0 or g_1.
-
+# Using a 2d matrix here, to keep the code intuitive.
+# TODO: Use sparse matrix instead.
 # y = 0:
-B[0,:n] = g_0(xy[0])
+B[0,:Nx] = g_0(xy[0])
 
 # x = 0 and x = 1:
 B[:,0] = g_0(xy[:,0])
 B[:,-1] = g_0(B[:,-1])
 
 # y = 1:
-B[-1,:] = g_1(xy[0,-1,:])
+B[-1,:] = g_1(xy[0,-1,:])/hx**2
 #print("B: \n", np.round(B, 2))
 
 b = B.flatten()
@@ -82,7 +86,7 @@ b = B.flatten()
 #print(B)
 #print(b)
 
-U = np.linalg.solve(A.toarray(), b[::-1]).reshape((n,n))
+U = np.linalg.solve(A.toarray(), b[::-1]).reshape((Ny,Nx))
 
 print(np.round(U,2))
 
