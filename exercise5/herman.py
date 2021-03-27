96d2d9c014290b8468f118263f5350cc71f6dd5c
Herman Sletmoen <hermansletmoen@gmail.com>
2021-03-27 14:33:38 +0100

    Added ex 5 AMR plots

--- exercise5/herman.py
+++ exercise5/herman.py
@@ -6,8 +6,23 @@ import matplotlib.cm
 import scipy.integrate
 import sympy
 
+def write_table_to_file(path, table, headers):
+    np.savetxt(path, table, header=" ".join(headers), comments="")
+    print(f"Wrote to {path}")
+
+def write_columns_to_file(path, columns, headers):
+    max_length = np.max([len(column) for column in columns])
+    for i in range(0, len(columns)):
+        length = len(columns[i])
+        column = np.full(max_length, np.nan)
+        column[0:length] = columns[i]
+        columns[i] = column
+    write_table_to_file(path, np.transpose(columns), headers)
+
 class Problem:
-    def __init__(self, f, x1x2, u1u2):
+    def __init__(self, f, x1x2, u1u2, label=""):
+        self.label = label
+
         self.x1, self.x2 = x1x2
         self.u1, self.u2 = u1u2
 
@@ -19,10 +34,13 @@ class Problem:
         A = (self.u1-up.subs(xvar,self.x1)+self.u2-up.subs(xvar,self.x2))/2
         B = (self.u1-up.subs(xvar,self.x1)-self.u2+up.subs(xvar,self.x2))/(2*self.x1) if self.x1 != 0 else (self.u2-up.subs(xvar,self.x2)-self.u1+up.subs(xvar,self.x1))/(2*self.x2)
         u = A + B*xvar + up # general solution
+        print(u)
 
         # make callable and efficient
         self.u = sympy.lambdify(xvar, u, "numpy")
         self.f = sympy.lambdify(xvar, f, "numpy")
+
+        self.strategy = None
 
     def solve_uniform(self, M):
         # TODO: make M vs M + 2 consistent
@@ -39,9 +57,8 @@ class Problem:
     def plot(self, show=False):
         fig, (ax1, ax2) = plt.subplots(2, 1)
 
-        errors = self.get_errors()
         widths = self.x[1:] - self.x[:-1]
-        ax1.bar(self.x[:-1], errors, width=widths, align="edge", edgecolor="black")
+        ax1.bar(self.x[:-1], self.errors, width=widths, align="edge", edgecolor="black")
 
         x = np.linspace(self.x1, self.x2, 500)
         ax2.plot(x, self.u(x), color="black", linewidth=5, label="analytic")
@@ -85,60 +102,83 @@ class Problem:
         U = np.concatenate(([self.u1], U, [self.u2])) # restore boundary conditions
         self.x = x
         self.U = U
-        return U
 
-    def get_errors(self):
+        # Collect errors
         def error_measure(x1, x2):
             integrand = lambda y: (self.u(y) - np.interp(y, self.x, self.U))**2
             return scipy.integrate.quad(integrand, x1, x2)[0]
-        errors = [error_measure(self.x[i], self.x[i+1]) for i in range(0, len(self.x)-1)]
-        errors = np.array(errors)
-        return errors
+        self.errors = [error_measure(self.x[i], self.x[i+1]) for i in range(0, len(self.x)-1)]
+        self.errors = np.array(self.errors)
 
-    def refine(self, M0, refiner, steps):
+        if self.strategy == "avgerror":
+            self.referror = 0.99 * np.mean(self.errors)
+        elif self.strategy == "maxerror":
+            self.referror = 0.7 * np.max(self.errors)
+
+        return U
+
+    def refine(self, M0, refiner, steps, plot):
         x = np.linspace(self.x1, self.x2, M0) # initial uniform grid
         for step in range(0, steps):
             self.solve(x)
-            self.plot(show=True)
+            self.write("AMR")
+            if plot:
+                self.plot(show=True)
             x = refiner(x)
 
-    def refine_uniformly(self):
+    def refine_uniformly(self, plot=False):
         def refiner(x):
             return np.linspace(self.x1, self.x2, 2*(len(x)-1)+1)
-        self.refine(2, refiner, 10)
+        self.refine(2, refiner, 10, plot)
 
-    def refine_adaptively(self, strategy, M0=2, steps=4):
+    def refine_adaptively(self, strategy, M0=2, steps=4, plot=False):
+        self.strategy = strategy
+
         def refiner(x):
-            errors = self.get_errors()
-
-            referror = None
-            if strategy == "avgerror":
-                referror = 0.99 * np.mean(errors)
-            elif strategy == "maxerror":
-                referror = 0.7 * np.max(errors)
-            else:
-                raise "Unknown refinement strategy"
-
             newx = np.array([])
             for i in range(0, len(x)-1):
                 newx = np.append(newx, x[i])
-                if errors[i] >= referror:
+                if self.errors[i] >= self.referror:
                     newx = np.append(newx, (x[i] + x[i+1]) / 2)
             newx = np.append(newx, x[-1])
             return newx
         
-        self.refine(M0, refiner, steps)
+        self.refine(M0, refiner, steps, plot)
+
+    def write(self, dir, normalize_errors=True):
+        M = len(self.x)
+        path = f"../report/exercise5/data/{dir}/{self.label}-M{M}.dat"
+        errors = self.errors / np.max(self.errors) if normalize_errors else self.errors
+        columns = [self.x, self.U, np.concatenate((errors, [0]))]
+        headers = [   "x",    "U",                           "E"]
+        if self.strategy is not None:
+            referror = self.referror / np.max(self.errors) if normalize_errors else self.referror
+            columns.append(np.full(len(self.x), referror))
+            headers.append("refE")
+        write_columns_to_file(path, columns, headers)
+        
 
 x = sympy.var("x")
 
-f, (x1, x2), (u1, u2) = -2, (0, 1), (0, 1)
-# f, (x1, x2), (u1, u2) = (40000*x**2 - 200) * sympy.exp(-100*x**2), (-1, +1), (np.exp(-100), np.exp(-100))
-# f, (x1, x2), (u1, u2) = (4000000*x**2 - 2000) * sympy.exp(-1000*x**2), (-1, +1), (np.exp(-1000), np.exp(-1000))
-# f, (x1, x2), (u1, u2) = 2/9*x**(-4/3), (0, 1), (0, 1) # TODO: how to deal with singularity at f(0)?
+#label, f, (x1, x2), (u1, u2) = "f1", -2, (0, 1), (0, 1)
+#label, f, (x1, x2), (u1, u2) = "f2", (40000*x**2 - 200) * sympy.exp(-100*x**2), (-1, +1), (np.exp(-100), np.exp(-100))
+#label, f, (x1, x2), (u1, u2) = "f3", (4000000*x**2 - 2000) * sympy.exp(-1000*x**2), (-1, +1), (np.exp(-1000), np.exp(-1000))
+#label, f, (x1, x2), (u1, u2) = "f4", 2/9*x**(-4/3), (0, 1), (0, 1) # TODO: how to deal with singularity at f(0)?
 
-prob = Problem(f, (x1, x2), (u1, u2))
+params = [
+    ("f1", -2, (0, 1), (0, 1)),
+    ("f2", (40000*x**2 - 200) * sympy.exp(-100*x**2), (-1, +1), (np.exp(-100), np.exp(-100))),
+    ("f3", (4000000*x**2 - 2000) * sympy.exp(-1000*x**2), (-1, +1), (np.exp(-1000), np.exp(-1000))),
+    ("f4", 2/9*x**(-4/3), (0, 1), (0, 1)), # TODO: how to deal with singularity at f(0)?
+]
+
+probs = (Problem(f, (x1, x2), (u1, u2), label=label) for label, f, (x1, x2), (u1, u2) in params)
+for prob in probs:
+    prob.refine_adaptively("avgerror", M0=20, steps=4, plot=False)
+
+# prob = Problem(f, (x1, x2), (u1, u2), label=label)
 #prob.solve_adaptive(np.array([-1, -0.5, 0, 0.1, 0.3, 0.8, 1]))
-prob.refine_adaptively("avgerror", M0=20, steps=3)
+#prob.refine_adaptively("avgerror", M0=20, steps=4, plot=False)
 #prob.refine_uniformly()
 #prob.solve_uniform(80)
 #prob.plot(show=True)
